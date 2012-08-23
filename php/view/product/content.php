d876d2afebdc9e20d0c954f8d28dfba5b5bd3597
david <david@shic.fr>
2012-08-23 20:15:55 +0200

    multi columns

--- php/view/product/content.php
+++ php/view/product/content.php
@@ -2,7 +2,7 @@
 
 $generator = new LoremIpsumGenerator;
 //100 words in html format
-$txt=$generator->getContent(200);
+$txt=$generator->getContent(rand(100,400),'plain');
 
 $title=$generator->getContent(rand(1,4), NULL, false);
 //100 words without any formatting
@@ -10,9 +10,16 @@ $title=$generator->getContent(rand(1,4), NULL, false);
 //100 words with 'text' formatting
 //$generator->getContent(100, 'txt');
 ?>
-<div class="pb1 " data-section="section-<?=$id?>">
+
+	
+
+<div class="pb1 " data-section="section-<?=$id?>" >
     <h3  class="fs1 bb"><?=$title?></h3>
-    <p class="mt1">
+<div class="row">
+    <div class="span8">
+    <p class="mt1 cols">
 	<?=$txt?>
     </p>
+        </div>
+</div>
 </div >
